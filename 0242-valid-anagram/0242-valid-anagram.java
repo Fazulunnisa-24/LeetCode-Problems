2acf89e269ada8772c55805522a258370413b628
Meer Fazulunnisa Begum <meernahid53@gmail.com>
2026-09-16 17:32:01 +0530

    Time: 3 ms (94.04%), Space: 44.4 MB (74.24%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] c=new int[26];
+        for(int i=0;i<s.length();i++){
+            int in=s.charAt(i)-'a';
+            c[in]++;
+        }
+        for(int i=0;i<t.length();i++){
+            int in=t.charAt(i)-'a';
+            c[in]--;
+        }
+        for(int i=0;i<26;i++){
+            if(c[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
